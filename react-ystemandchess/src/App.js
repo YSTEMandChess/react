c4bc2c985202dddd2afc37d413beb900847f60b8
Brandon Lingo <brandonlingo0@gmail.com>
2024-02-05 10:52:30 -0800

    add react-router-dom

--- react-ystemandchess/src/App.js
+++ react-ystemandchess/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
