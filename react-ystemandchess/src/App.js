3e646427b4ce2a8d81ec02fac69cdf6b02cf9200
Brandon Lingo <brandonlingo0@gmail.com>
2024-02-05 12:34:34 -0800

    add navbar

--- react-ystemandchess/src/App.js
+++ react-ystemandchess/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import NavBar from "./NavBar/NavBar";
 
 function App() {
   return (
-
     // All components need to be wrapped with the '<Router>' tag
     <Router>
       <div className="App">
+        <NavBar>
 
+        </NavBar>
+        <Routes>
+          
+        </Routes>
       </div>
     </Router>
   );
---
+++ react-ystemandchess/src/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import './NavBar.css'
+import { Link } from 'react-router-dom';
+import FullLogo from '../images/full_logo.png'
+
+const NavBar = () => {
+    return ( 
+       <header className="nav-bar">
+
+        <Link to='#'>
+            <img src={FullLogo}></img>
+        </Link>
+
+        <div className="nav-links">
+            <Link to='#' className='links'>Programs</Link>
+            <Link to='#' className='links'>About us </Link>
+            <Link to='#' className='links'>Lessons/Play</Link>
+            <Link to='#' className='links'>Login</Link>
+        </div>
+       </header>
+     );
+}
+ 
+export default NavBar;
