f9104584ef7f0563e685286d0c33ce680b45b51a
Brandon Lingo <brandonlingo0@gmail.com>
2024-02-07 13:22:59 -0800

    add cards to sector 2

--- react-ystemandchess/src/Pages/Home/Home.js
+++ react-ystemandchess/src/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import "./Home.css";
 import LogoLineBr from "../../images/LogoLineBreak.png";
-import TreesGroup from "../../images/Trees-Group.png"
+import TreesGroup from "../../images/Trees-Group.png";
 
 const Home = () => {
   return (
@@ -13,18 +13,30 @@ const Home = () => {
           </h1>
 
           <p>
-            We are a nonprofit organization empowering <br></br>children to find their
-            own success in STEM through <br></br>Chess, Math and Computer Science.
+            We are a nonprofit organization empowering <br></br>children to find
+            their own success in STEM through <br></br>Chess, Math and Computer
+            Science.
           </p>
 
-          <button className="donate-button"><strong>Donate</strong></button>
+          <button className="donate-button">
+            <strong>Donate</strong>
+          </button>
         </div>
         <div className="pic">
-            <img src={TreesGroup} id="tree-group-img"></img>
+          <img src={TreesGroup} id="tree-group-img"></img>
         </div>
       </div>
       <img src={LogoLineBr} className="logo-break"></img>
-      <div className="home-content2"></div>
+      <h1 id="floating-h1">Everyone is included.</h1>
+      <h1 id="floating-h1">Everyone is welcomed.</h1>
+      <div className="home-content2">
+        <div className="card1">
+            
+        </div>
+        <div className="card2">
+
+        </div>
+      </div>
     </div>
   );
 };
