5dfb32e0220f6858e6bb9f0edda621a98863c3f2
Brandon Lingo <brandonlingo0@gmail.com>
2024-02-07 13:04:45 -0800

    add sector 1 of home page

--- react-ystemandchess/src/App.js
+++ react-ystemandchess/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import NavBar from "./NavBar/NavBar";
+import Home from "./Pages/Home/Home";
+import Programs from "./Pages/Programs/Programs";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
 
         </NavBar>
         <Routes>
+
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/programs" element={<Programs></Programs>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
           
         </Routes>
       </div>
--- react-ystemandchess/src/Pages/Home/Home.js
+++ react-ystemandchess/src/Pages/Home/Home.js
@@ -1,7 +1,32 @@
+import "./Home.css";
+import LogoLineBr from "../../images/LogoLineBreak.png";
+import TreesGroup from "../../images/Trees-Group.png"
+
 const Home = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  return (
+    <div className="home-container">
+      <div className="home-content1">
+        <div className="info">
+          <h1>
+            Helping your child develop <br />
+            critical thinking skills!
+          </h1>
+
+          <p>
+            We are a nonprofit organization empowering <br></br>children to find their
+            own success in STEM through <br></br>Chess, Math and Computer Science.
+          </p>
+
+          <button className="donate-button"><strong>Donate</strong></button>
+        </div>
+        <div className="pic">
+            <img src={TreesGroup} id="tree-group-img"></img>
+        </div>
+      </div>
+      <img src={LogoLineBr} className="logo-break"></img>
+      <div className="home-content2"></div>
+    </div>
+  );
+};
+
 export default Home;
