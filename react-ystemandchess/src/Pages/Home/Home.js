78dabc15372f41b022ec32ef8d8ead7f26671036
Brandon Lingo <brandonlingo0@gmail.com>
2024-02-07 19:52:12 -0800

    Add sections 2 and 3 of homepage

--- react-ystemandchess/src/Pages/Home/Home.js
+++ react-ystemandchess/src/Pages/Home/Home.js
@@ -1,9 +1,19 @@
 import "./Home.css";
 import LogoLineBr from "../../images/LogoLineBreak.png";
 import TreesGroup from "../../images/Trees-Group.png";
+import Heart from '../../images/heart-regular.svg'
+import Gem from '../../images/gem-regular.svg'
+import LargeInfo from '../../images/large_info.png'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-  return (
+
+    // Sends the user to donate page, when donate button is clicked
+    const handleDonateButton = () =>{
+        window.location.href = 'https://donorbox.org/y-stem-and-chess-inc-learning-platform'
+    }
+
+    return (
     <div className="home-container">
       <div className="home-content1">
         <div className="info">
@@ -18,7 +28,7 @@ const Home = () => {
             Science.
           </p>
 
-          <button className="donate-button">
+          <button className="donate-button" onClick={handleDonateButton}>
             <strong>Donate</strong>
           </button>
         </div>
@@ -31,11 +41,22 @@ const Home = () => {
       <h1 id="floating-h1">Everyone is welcomed.</h1>
       <div className="home-content2">
         <div className="card1">
-            
+            <img src={Heart}></img>
+            <h1>Free</h1>
+            <p>For students who qualify for <br></br> free and reduced lunch.<br></br>
+            Our lessons are free.</p>
+            <button><strong>Join Now!</strong></button>
         </div>
         <div className="card2">
-
+            <img src={Gem}></img>
+            <h1>Premium</h1>
+            <p>For students who don't qualify <br></br> for free and reduced lunch. <br></br>
+            $25 / Week <br></br> First lesson is FREE. <br></br> Cancel anytime.</p>
+            <button><strong>Join Now!</strong></button>
         </div>
+      </div>
+      <div className="home-content3">
+        <img src={LargeInfo}></img>
       </div>
     </div>
   );
