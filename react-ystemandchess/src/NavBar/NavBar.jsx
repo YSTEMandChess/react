f40e5b3a71e5f2b44d2c36f6b232af8f83f4531c
Duy-B-Nguyen <duybnguyen04@gmail.com>
2024-02-24 17:35:02 -0500

    add animations to navbar

--- react-ystemandchess/src/NavBar/NavBar.jsx
+++ react-ystemandchess/src/NavBar/NavBar.jsx
@@ -1,49 +1,155 @@
-import './NavBar.scss'
-import { Link } from 'react-router-dom';
-import FullLogo from '../images/full_logo.png'
-import { useState } from 'react';
+import React, { useState, useEffect, useRef } from "react"; // Import useEffect and useRef
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import FullLogo from "../images/full_logo.png";
+import "./NavBar.scss";
+
+const navbarVariants = {
+  parentInitial: {
+    opacity: 0,
+    translateY: -25,
+  },
+  parentAnimate: {
+    opacity: 1,
+    translateY: -10,
+    transition: {
+      duration: 0.3,
+    },
+  },
+
+  childInitial: {
+    opacity: 0,
+    translateY: -25,
+  },
+  childAnimate: {
+    opacity: 1,
+    translateY: 0,
+    transition: {
+      duration: 0.3,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const NavBar = () => {
-  const [dropdown, setDropdown] = useState(false)
+  const [dropdown, setDropdown] = useState(false);
+  const dropdownRef = useRef(null);
 
   const handleDropdown = () => {
-    setDropdown(prevDropdown => !prevDropdown)
-  }
-  return ( 
-      <header className="nav-bar">
+    setDropdown((prevDropdown) => !prevDropdown);
+  };
 
-      <Link to='/'>
-          <img src={FullLogo}></img>
+  useEffect(() => {
+    const closeDropdown = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", closeDropdown);
+
+    return () => {
+      document.removeEventListener("mousedown", closeDropdown);
+    };
+  }, []);
+
+  return (
+    <header className="nav-bar">
+      <Link to="/">
+        <img src={FullLogo} alt="Logo" />
       </Link>
 
       <div className="nav-links">
-          <Link to='/programs' className='links'>Programs</Link>
-          <div className="dropdown">
-            <p onClick={handleDropdown}>About Us</p>
-            {dropdown && 
-            <div className='dropdown-menu'>
-              <div className='education'>
+        <Link to="/programs" className="links">
+          Programs
+        </Link>
+        <div className="dropdown" ref={dropdownRef}>
+          {" "}
+          <p onClick={handleDropdown} className="links">
+            About Us
+          </p>
+          {dropdown && (
+            <motion.div
+              className="dropdown-menu"
+              initial="parentInitial"
+              animate="parentAnimate"
+              variants={navbarVariants}
+            >
+              <motion.div
+                className="education"
+                initial="childInitial"
+                animate="childAnimate"
+                variants={navbarVariants}
+              >
                 <h3>Education</h3>
-                <Link to="/benefit-of-computer-science">Benefit of Computer Science</Link>
-                <Link to="/benefit-of-chess">Benefit of Chess</Link>
-                <Link to="/benefit-of-math-tutoring">Benefit of Math Tutoring</Link>
-                <Link to="/benefit-of-mentoring">Benefit of Mentoring</Link>
-              </div>
-              <div className="what-we-do">
+                <Link to="/benefit-of-computer-science">
+                  <motion.h4 variants={navbarVariants}>
+                    Benefit of Computer Science
+                  </motion.h4>
+                </Link>
+                <Link to="/benefit-of-chess">
+                  {" "}
+                  <motion.h4 variants={navbarVariants}>
+                    Benefit of Chess
+                  </motion.h4>
+                </Link>
+                <Link to="/benefit-of-math-tutoring">
+                  <motion.h4 variants={navbarVariants}>
+                    Benefit of Math Tutoring
+                  </motion.h4>
+                </Link>
+                <Link to="/benefit-of-mentoring">
+                  {" "}
+                  <motion.h4 variants={navbarVariants}>
+                    Benefit of Mentoring
+                  </motion.h4>
+                </Link>
+              </motion.div>
+              <motion.div
+                className="what-we-do"
+                initial="childInitial"
+                animate="childAnimate"
+                variants={navbarVariants}
+              >
                 <h3>What We Do</h3>
-                <Link>Online Expansion</Link>
-                <Link>About Us</Link>
-                <Link>Mission</Link>
-                <Link>Financial</Link>
-                <Link>Board</Link>
-              </div>
-            </div>}
-          </div>
-          <Link to='#' className='links'>Lessons/Play</Link>
-          <Link to='/login' className='links'>Login</Link>
+                <Link>
+                  <motion.h4 variants={navbarVariants}>
+                    Online Expansion
+                  </motion.h4>
+                </Link>
+                <Link>
+                  <motion.h4 variants={navbarVariants}>
+                    Online Expansion
+                  </motion.h4>
+                </Link>
+                <Link>
+                  <motion.h4 variants={navbarVariants}>
+                    Online Expansion
+                  </motion.h4>
+                </Link>
+                <Link>
+                  <motion.h4 variants={navbarVariants}>
+                    Online Expansion
+                  </motion.h4>
+                </Link>
+                <Link>
+                  <motion.h4 variants={navbarVariants}>
+                    Online Expansion
+                  </motion.h4>
+                </Link>
+              </motion.div>
+            </motion.div>
+          )}
+        </div>
+        <Link to="#" className="links">
+          Lessons/Play
+        </Link>
+        <Link to="/login" className="links">
+          Login
+        </Link>
       </div>
-      </header>
-    );
-}
- 
+    </header>
+  );
+};
+
 export default NavBar;
