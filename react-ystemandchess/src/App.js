f1a4dd5ac2527858bfb0c8080485ace1bc4c08a6
aedrus <marioferrera10@gmail.com>
2024-03-01 19:22:11 -0500

    feat: add benefit pages
    Add components and stylesheets for CS, Chess, Math Tutoring, and Mentoring pages. Small refactor to stylesheets and HTML syntax to align with accessibility standards. Initial Routes defined in entrypoint (App.js).

--- react-ystemandchess/src/App.js
+++ react-ystemandchess/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import NavBar from "./NavBar/NavBar";
 import Home from "./Pages/Home/Home";
 import Programs from "./Pages/Programs/Programs";
+import CSBenefitPage from "./Pages/About-Us/Benefit-of-CS/CSBenefitPage";
+import ChessBenefitPage from "./Pages/About-Us/Benefit-of-Chess/ChessBenefitPage";
+import MathTutBenefitPage from "./Pages/About-Us/Benefit-of-Math-tut/MathTutBenefitPage";
+import MentoringBenefitPage from "./Pages/About-Us/Benefit-of-Mentoring/MentoringBenefitPage";
 import Login from "./Pages/Login/Login";
 import SignUp from "./Pages/SignUp/SignUp"
 import Footer from "./Footer/Footer";
@@ -21,6 +25,10 @@ function App() {
           <Route path="/programs" element={<Programs></Programs>}>
             <Route></Route>
           </Route>
+          <Route path="/benefit-of-computer-science" element={<CSBenefitPage></CSBenefitPage>}></Route>
+          <Route path="/benefit-of-chess" element={<ChessBenefitPage></ChessBenefitPage>}></Route>
+          <Route path="/benefit-of-math-tutoring" element={<MathTutBenefitPage></MathTutBenefitPage>}></Route>
+          <Route path="/benefit-of-mentoring" element={<MentoringBenefitPage></MentoringBenefitPage>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
