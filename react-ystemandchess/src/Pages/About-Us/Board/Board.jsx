0bb80d0346842eb497c4f9a3f0c8d9b4c72ba07a
Chloe <chloekeyiw@gmail.com>
2024-05-20 15:15:09 -0400

    add: board page

---
+++ react-ystemandchess/src/Pages/About-Us/Board/Board.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Board.scss';
+import LogoLineBr from '../../../images/LogoLineBreak.png';
+
+const Board = () => {
+  return (
+    <div className="board-container">
+      <h1>Board</h1>
+      <div className="line-break">
+        <img src={LogoLineBr} alt="Line Break" />
+      </div>
+      <div className="board-content">
+        <div className="section">
+          <h2>Officers</h2>
+          <ul>
+            <li>
+              <name className="name">Devin Nakano</name><br />
+              Founder, President and Executive Director
+            </li>
+            <li>
+              <name className="name">Jasmine Redlich</name><br />
+              Vice President
+            </li>
+            <li>
+              <name className="name">Owen Oertell</name><br />
+              Secretary
+            </li>
+            <li>
+              <name className="name">Kelsey Korvela</name><br />
+              Treasurer
+            </li>
+          </ul>
+        </div>
+        <div className="section">
+          <h2>Board Members</h2>
+          <ul>
+            <li>
+              <name className="name">Amit Jain, Phd</name><br />
+              Chair of the Computer Science Boise State University
+            </li>
+            <li>
+              <name className="name">Sasikanth R.</name><br />
+              International Board Member and Entrepreneur
+            </li>
+            <li>
+              <name className="name">Holly Trainer</name><br />
+              Retired Public School teacher
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
