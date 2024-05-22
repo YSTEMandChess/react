129e6407c09c68eb76addc9548a9f7501141ff52
Bit by Bit <technologybitbybit@gmail.com>
2024-05-22 12:04:31 -0500

    financials page and mentor page

--- react-ystemandchess/src/Pages/About-Us/Financial/Financial.jsx
+++ react-ystemandchess/src/Pages/About-Us/Financial/Financial.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './Financial.scss';
 import LogoLineBr from '../../../images/LogoLineBreak.png';
 
-
 const Financial = () => {
   return (
-    <div className="board-container">
+    <div className="financial-content">
       <h1>Financials</h1>
       <div className="line-break">
         <img src={LogoLineBr} alt="Line Break" />
+      </div>
+      <div className="financial-grid">
         <div className="financial-item">
           <h3>2018 Financials</h3>
           <button className="view-button">View Here</button>
