359b1a1ba2d2d686ad32af423d7e76bde143608c
Bit by Bit <technologybitbybit@gmail.com>
2024-05-23 11:45:43 -0500

    financial and mentor pages are now responsive

--- react-ystemandchess/src/Pages/Mentor/Mentor.jsx
+++ react-ystemandchess/src/Pages/Mentor/Mentor.jsx
@@ -6,26 +6,31 @@ import volunteerImg from "../../images/volunteer.png";
 import teacher from "../../images/teaching.png";
 import makeADifference from "../../images/difference.png";
 
-
-
 const Mentor = () => {
   return (
     <div className="board-container">
-      <h1>Become a Mentor</h1>
-      <p>Your time and talent can make a real difference in people's lives.</p>
-      <button className="apply-button">Apply Now</button>
-      <img src={cabbageImg} alt="Line Break" /> 
+      <div className="flex-container">
+        <div className="text-content">
+          <h1>Become a Mentor</h1>
+          <p>Your time and talent can make a real difference in people's lives.</p>
+          <button className="apply-button">Apply Now</button>
+        </div>
+        <div className="image-content">
+          <img src={cabbageImg} alt="Cabbage" />
+        </div>
+      </div>
+
       <div className="line-break">
-        <img src={LogoLineBr} alt="Line Break" /> </div>
-     
-      
+        <img src={LogoLineBr} alt="Line Break" />
+      </div>
+
       <div className="mentor-details">
-        <img src={makeADifference} alt="Line Break" /> 
+        <img src={makeADifference} alt="Make a Difference" />
       </div>
 
       <div className="mentor-roles">
-        <img src={volunteerImg} alt="Line Break" /> 
-        <img src={teacher} alt="Line Break" /> 
+        <img src={volunteerImg} alt="Volunteer" />
+        <img src={teacher} alt="Teaching and Learning" />
       </div>
 
       <button className="apply-button">Apply Now</button>
