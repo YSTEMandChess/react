4703f4e86ce80e385a08e959ae4a9f8eacf277a0
aedrus <marioferrera10@gmail.com>
2024-05-29 13:00:23 -0500

    feat: add student inventory page MVP and app routes refactor
    Add base form of the student inventory page to the platform. Fix a small bug related to footer component. Add small refactor to app routing by abstracting it into a seperate module.

--- react-ystemandchess/src/App.js
+++ react-ystemandchess/src/App.js
@@ -1,52 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import NavBar from "./NavBar/NavBar";
-import Home from "./Pages/Home/Home";
-import Programs from "./Pages/Programs/Programs";
-import CSBenefitPage from "./Pages/About-Us/Benefit-of-CS/CSBenefitPage";
-import ChessBenefitPage from "./Pages/About-Us/Benefit-of-Chess/ChessBenefitPage";
-import MathTutBenefitPage from "./Pages/About-Us/Benefit-of-Math-tut/MathTutBenefitPage";
-import MentoringBenefitPage from "./Pages/About-Us/Benefit-of-Mentoring/MentoringBenefitPage";
-import Lessons from "./Pages/Lessons/Lessons";
-import Login from "./Pages/Login/Login";
-import SignUp from "./Pages/SignUp/SignUp"
 import Footer from "./Footer/Footer";
-import Mission from "./Pages/About-Us/Mission/Mission";
-import SponsorsPartners from "./Pages/About-Us/SponsorsPartners/SponsorsPartners";
-import Board from "./Pages/About-Us/Board/Board";
-import Mentor from "./Pages/Mentor/Mentor";
-import Financial from "./Pages/About-Us/Financial/Financial";
+import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
-    // All components need to be wrapped with the '<Router>' tag
     <Router>
       <div className="App">
-        <NavBar>
-
-        </NavBar>
-        <Routes>
-
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/programs" element={<Programs></Programs>}>
-            <Route></Route>
-          </Route>
-          <Route path="/benefit-of-computer-science" element={<CSBenefitPage></CSBenefitPage>}></Route>
-          <Route path="/benefit-of-chess" element={<ChessBenefitPage></ChessBenefitPage>}></Route>
-          <Route path="/benefit-of-math-tutoring" element={<MathTutBenefitPage></MathTutBenefitPage>}></Route>
-          <Route path="/benefit-of-mentoring" element={<MentoringBenefitPage></MentoringBenefitPage>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/mission" element = {<Mission></Mission>}></Route>
-          <Route path="/mentor" element = {<Mentor></Mentor>}></Route>
-          <Route path="/financial" element = {<Financial></Financial>}></Route>
-          <Route path="/lessons" element = {<Lessons></Lessons>}></Route>
-          <Route path="/sponsors&partners" element = {<SponsorsPartners></SponsorsPartners>}></Route>
-          <Route path="/board" element = {<Board></Board>}></Route>
-        </Routes>
+        <NavBar></NavBar>
+        <AppRoutes />
+        <Footer></Footer>
       </div>
-
-      <Footer></Footer>
     </Router>
   );
 }
---
+++ react-ystemandchess/src/Pages/Student-Inventory/StudentInventory.jsx
@@ -0,0 +1,180 @@
+import React from 'react';
+import './StudentInventory.scss';
+
+// Image Imports
+import userPortraitCamera from "../../images/camera.svg"
+import lineGraphPlaceholder from "../../images/line-graph-placeholder.png"
+import activityIcon from "../../images/StudentInventoryIcons/activity-icon.svg"
+import mentorIcon from "../../images/StudentInventoryIcons/mentor-icon.svg"
+import learningIcon from "../../images/StudentInventoryIcons/learning-icon.svg"
+import chessLessonsIcon from "../../images/StudentInventoryIcons/chess-lessons-icon.svg"
+import gamesIcon from "../../images/StudentInventoryIcons/games-icon.svg"
+import puzzlesIcon from "../../images/StudentInventoryIcons/puzzles-icon.svg"
+import playComputerIcon from "../../images/StudentInventoryIcons/play-computer-icon.svg"
+import recordingsIcon from "../../images/StudentInventoryIcons/recordings-icon.svg"
+import backpackIcon from "../../images/StudentInventoryIcons/backpack-icon.svg"
+
+const StudentInventory = ({ userPortraitSrc, userName }) => {
+  return (
+    <main id="main-inventory-content">
+        <section className="inv-intro">
+            <div className="inv-intro-portrait">
+                <img className="inv-intro-portrait-face" src={userPortraitSrc} alt="user portrait"></img>
+                <img className="inv-intro-portrait-camera" src={userPortraitCamera} alt="user portrait camera icon"></img>
+            </div>
+            <div className="inv-intro-welcome">
+                <h1>Hello, {userName}!</h1>
+            </div>
+        </section>
+        <section className="inv-inventory">
+            <div className="inv-inventory-topbar">
+                <h2>Your Progress</h2>
+            </div>
+            <div className="inv-inventory-analytics">
+                <div className="inv-inventory-analytics-graph">
+                    <img src={lineGraphPlaceholder} alt="progress graph" />
+                </div>
+                <div className="inv-inventory-analytics-metrics">
+                    <h3>Time Spent</h3>
+                    <ul>
+                        <li>Website: <strong>45 Min</strong></li>
+                        <li>Playing: <strong>15 Min</strong></li>
+                        <li>Lessons: <strong>10 Min</strong></li>
+                        <li>Puzzle: <strong>5 Min</strong></li>
+                        <li>Mentoring: <strong>10 Min</strong></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="inv-inventory-content-section">
+                <nav className="inv-inventory-content-tabs">
+                    <ul>
+                        <div className="inventory-tab active-tab">
+                            <img src={activityIcon} alt="activity icon"></img>
+                            <li data-content="activity">Activity</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={mentorIcon} alt="mentor icon"></img>
+                            <li data-content="mentor">Mentor Session</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={learningIcon} alt="learning icon"></img>
+                            <li data-content="learning">Learning</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={chessLessonsIcon} alt="chess lessons icon"></img>
+                            <li data-content="lessons">Chess Lessons</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={gamesIcon} alt="games icon"></img>
+                            <li data-content="games">Games</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={puzzlesIcon} alt="puzzles icon"></img>
+                            <li data-content="puzzles">Puzzles</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={playComputerIcon} alt="play with computer icon"></img>
+                            <li data-content="computer">Play With Computer</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={recordingsIcon} alt="recordings icon"></img>
+                            <li data-content="recordings">Recordings</li>
+                        </div>
+                        <div className="inventory-tab">
+                            <img src={backpackIcon} alt="backpack icon"></img>
+                            <li data-content="backpack">Backpack Inventory</li>
+                        </div>
+                    </ul>
+                </nav>
+                <div className="inv-inventory-content-content">
+                    <div id="inventory-content-activity" class="inventory-content active-content">
+                        <div className="inventory-content-headingbar">
+                            <h2>Activity</h2>
+                            <h4>May 2024</h4>
+                        </div>
+                        <div className="inventory-content-body">
+                            <div className="inventory-content-line"></div>
+                            <article className="inventory-content-timecard">
+                                <div className="inventory-content-col1"></div>
+                                <div className="inventory-content-col2">
+                                    <p>May 24 2024</p>
+                                    <p>7:00 PM</p>
+                                </div>
+                                <div className="inventory-content-col3">
+                                    <p>Solved 2 tactical puzzles.</p>
+                                </div>
+                            </article>
+                            <article className="inventory-content-timecard">
+                                <div className="inventory-content-col1"></div>
+                                <div className="inventory-content-col2">
+                                    <p>May 19 2024</p>
+                                    <p>3:00 PM</p>
+                                </div>
+                                <div className="inventory-content-col3">
+                                    <p>Practiced 7 positions on Piece Checkmates I.</p>
+                                </div>
+                            </article>
+                            <article className="inventory-content-timecard">
+                                <div className="inventory-content-col1"></div>
+                                <div className="inventory-content-col2">
+                                    <p>May 16 2024</p>
+                                    <p>4:00 PM</p>
+                                </div>
+                                <div className="inventory-content-col3">
+                                    <p>Completed 100 games of chess.</p>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                    <div id="inventory-content-mentor" class="inventory-content">
+                        <h2>Content 2</h2>
+                        <p>This is the content for Tab 2.</p>
+                    </div>
+                    <div id="inventory-content-learning" class="inventory-content">
+                        <h2>Content 3</h2>
+                        <p>This is the content for Tab 3.</p>
+                    </div>
+                    <div id="inventory-content-lessons" class="inventory-content">
+                        <h2>Content 4</h2>
+                        <p>This is the content for Tab 4.</p>
+                    </div>
+                    <div id="inventory-content-games" class="inventory-content">
+                        <h2>Content 4</h2>
+                        <p>This is the content for Tab 4.</p>
+                    </div>
+                    <div id="inventory-content-puzzles" class="inventory-content">
+                        <h2>Content 4</h2>
+                        <p>This is the content for Tab 4.</p>
+                    </div>
+                    <div id="inventory-content-computer" class="inventory-content">
+                        <h2>Content 4</h2>
+                        <p>This is the content for Tab 4.</p>
+                    </div>
+                    <div id="inventory-content-recordings" class="inventory-content">
+                        <h2>Content 4</h2>
+                        <p>This is the content for Tab 4.</p>
+                    </div>
+                    <div id="inventory-content-backpack" class="inventory-content">
+                        <h2>Content 4</h2>
+                        <p>This is the content for Tab 4.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="inv-streaks">
+
+        </section>
+        <section className="inv-growth-quest">
+
+        </section>
+        <section className="inv-badges">
+
+        </section>
+        <section className="inv-leaderboard">
+
+        </section>
+    </main>
+  );
+};
+
+export default StudentInventory;
