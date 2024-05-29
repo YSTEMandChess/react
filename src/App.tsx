f8f503b47c71ee36673d47d86e87d9c5ba19d6ec
Noah (Axol) <168484999+DeMentidDev@users.noreply.github.com>
2024-05-29 18:57:44 +0000

    Codesandbox push; will be converting to match the formatting of the other branches

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,43 @@ import CSBenefitPage from "./Pages/About-Us/Benefit-of-CS/CSBenefitPage";
 import ChessBenefitPage from "./Pages/About-Us/Benefit-of-Chess/ChessBenefitPage";
 import MathTutBenefitPage from "./Pages/About-Us/Benefit-of-Math-tut/MathTutBenefitPage";
 import MentoringBenefitPage from "./Pages/About-Us/Benefit-of-Mentoring/MentoringBenefitPage";
+import UserProfile from "./Pages/User-Profile/User-Profile";
 import Login from "./Pages/Login/Login";
-import SignUp from "./Pages/SignUp/SignUp"
+import SignUp from "./Pages/SignUp/SignUp";
 import Footer from "./Footer/Footer";
 import Mission from "./Pages/About-Us/Mission/Mission";
-
 
 function App() {
   return (
     // All components need to be wrapped with the '<Router>' tag
     <Router>
       <div className="App">
-        <NavBar>
-
-        </NavBar>
+        <NavBar></NavBar>
         <Routes>
-
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/programs" element={<Programs></Programs>}>
             <Route></Route>
           </Route>
-          <Route path="/benefit-of-computer-science" element={<CSBenefitPage></CSBenefitPage>}></Route>
-          <Route path="/benefit-of-chess" element={<ChessBenefitPage></ChessBenefitPage>}></Route>
-          <Route path="/benefit-of-math-tutoring" element={<MathTutBenefitPage></MathTutBenefitPage>}></Route>
-          <Route path="/benefit-of-mentoring" element={<MentoringBenefitPage></MentoringBenefitPage>}></Route>
+          <Route
+            path="/benefit-of-computer-science"
+            element={<CSBenefitPage></CSBenefitPage>}
+          ></Route>
+          <Route
+            path="/benefit-of-chess"
+            element={<ChessBenefitPage></ChessBenefitPage>}
+          ></Route>
+          <Route
+            path="/benefit-of-math-tutoring"
+            element={<MathTutBenefitPage></MathTutBenefitPage>}
+          ></Route>
+          <Route
+            path="/benefit-of-mentoring"
+            element={<MentoringBenefitPage></MentoringBenefitPage>}
+          ></Route>
           <Route path="/login" element={<Login></Login>}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/mission" element = {<Mission></Mission>}></Route>
+          <Route path="/mission" element={<Mission></Mission>}></Route>
+          <Route path="/user-profile" element={<UserProfile />}></Route>
         </Routes>
       </div>
 
