64039adb120c0254f74c2f7d714259ad3d2b83be
Harsha Saketh Konjeti <hkonjeti@Harshas-MacBook-Air.local>
2024-07-19 20:33:08 +0530

    Header and Footer alignment changes.

--- react-ystemandchess/src/AppRoutes.js
+++ react-ystemandchess/src/AppRoutes.js
@@ -58,7 +58,7 @@ const AppRoutes = () => {
       <Route path="/student" element={<Student/>} />
       <Route path="/mentor-profile" element={<MentorProfile />} />
       <Route path="/student-profile" element={<UserProfile />} />
-      <Route path="/learnings" element={<LessonOverlay />} />
+      {/* <Route path="/learnings" element={<LessonOverlay />} /> */}
     </Routes>
   );
 };
