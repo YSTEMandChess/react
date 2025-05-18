277897f71ac1a05ab687f1db765964d19ee6a285
CharlotteZhang13 <zhangxiyue0128@outlook.com>
2025-05-18 20:41:47 +0800

    full NodeJS api

--- middlewareNode/routes/lessons.js
+++ middlewareNode/routes/lessons.js
@@ -84,8 +84,8 @@ router.get(
     const piece = decodeURIComponent(req.query.piece);
     const lessonNum = Number(decodeURIComponent(req.query.lessonNum));
 
-    if (!piece) {
-      return res.status(400).json("Error: 400. Please provide a piece.");
+    if (!piece || isNaN(lessonNum)) {
+      return res.status(400).json("Error: 400. Missing or invalid parameters.");
     }
 
     try {
@@ -109,6 +109,67 @@ router.get(
         }
       if (!found) return res.status(400).json("Error: 400. No more lessons left.");
       }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err.message });
+    } finally {
+      await client.close();
+    }
+  }
+);
+
+router.get(
+  "/updateLessonCompletion",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { username } = req.user;
+    const piece = decodeURIComponent(req.query.piece);
+    const lessonNum = Number(decodeURIComponent(req.query.lessonNum));
+
+    if (!piece || isNaN(lessonNum)) {
+      return res.status(400).json("Error: 400. Missing or invalid parameters.");
+    }
+    try {
+      await client.connect();
+      const db = client.db("ystem");
+      const users = db.collection("users");
+
+      const userDoc = await users.findOne(
+        { username: username },
+        { projection: { lessonsCompleted: 1 } }
+      );
+
+      if (!userDoc) throw new Error("User does not exist");
+      if (!userDoc.lessonsCompleted) throw new Error("User does not have lesson record");
+
+      let index = -1;
+      userDoc.lessonsCompleted.forEach((lesson, i) => {
+        if (lesson.piece === piece) {
+          index = i;
+        }
+      });
+
+      if (index === -1) {
+        return res.status(404).json("Piece not found in lessonsCompleted");
+      }
+
+      const updateResult = await users.updateOne(
+      { username },
+      {
+        $set: {
+          [`lessonsCompleted.${index}`]: {
+            piece,
+            lessonNumber: lessonNum + 1,
+          },
+        },
+      }
+    );
+    if (updateResult.modifiedCount > 0) {
+      res.status(200).json("Lesson progress updated");
+    } else {
+      res.status(304).json("No changes made");
+    }
+
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: err.message });
