a1888c5c35e29c42bcf9c5c1eb4d6efec962c191
narenDaw <narend@vt.edu>
2025-05-21 14:10:59 -0400

    Fixed homepage for more dynamic page build.

--- react-ystemandchess/src/Pages/Home/Home.tsx
+++ react-ystemandchess/src/Pages/Home/Home.tsx
@@ -62,11 +62,8 @@ const Home = () => {
           {/* <strong>Donate</strong> */}
           {/* </button> */}
 
-          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-1 text-lg font-medium text-white backdrop-blur-3xl">
+          <button className="donateButton">
               Donate
-            </span>
           </button>
         </div>
         <div className="pic">
@@ -84,8 +81,7 @@ const Home = () => {
         alt="line break"
       ></img>
 
-      <h1 id="floating-h1">Everyone is included.</h1>
-      <h1 id="floating-h1">Everyone is welcomed.</h1>
+      <h1 id="floating-h1">Everyone is included. Everyone is welcome.</h1>
 
       <div className="home-content2">
         <div className="card1">
