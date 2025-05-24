72f755fc186401cc498535a98b2a6cf7341a21b7
narenDaw <narend@vt.edu>
2025-05-23 23:45:53 -0400

    Finished Lesson Selection!

--- react-ystemandchess/src/Pages/LessonsSelection/LessonsSelection.jsx
+++ react-ystemandchess/src/Pages/LessonsSelection/LessonsSelection.jsx
@@ -1,64 +1,108 @@
 import "./LessonsStyle.css";
 import { useNavigate } from "react-router";
+import React, { useState, useEffect } from 'react';
+import { getScenarioLength, getScenario} from "../Lessons/Scenarios";
+
+
+// li onClick={() => navigate("/lessons", {state: {scenario: "Pawn", lesson: "Basic"}})}>Basic</li>
+
+function ScenarioTemplate({ scenario, onClick }) { // You can pass down references, like onClick!
+    return (
+      <div className="item-template" onClick={onClick}>
+        <div>Title: {scenario.name}</div>
+      </div>
+    );
+  }
+
+  function LessonTemplate({ lesson, onClick }) {
+    return (
+      <div className="item-template" onClick={onClick}>
+        <div>Title: {lesson.name}</div>
+      </div>
+    );
+  }
 
 export default function LessonSelection() {
-    const navigate = useNavigate();
+    const [showScenarios, setShowScenarios] = useState(false);
+    const [showLessons, setShowLessons] = useState(false);
+
+    const [selectedScenario, setSelectedScenario] = useState(null);
+    const [selectedLesson, setSelectedLesson] = useState(null);
+    
+    const [scenarios, setScenarios] = useState([]);
+    const [lessons, setLessons] = useState([]);
+
+    useEffect(() => {
+        const scenarioList = [];
+        for (let i = 0; i < getScenarioLength(); i++) {
+            scenarioList.push(getScenario(i));
+        }
+        setScenarios(scenarioList);
+    }, []);
+
+    const handleScenarioClick = (scenario) => {
+        setShowLessons(false);
+        setShowScenarios(false);
+        setSelectedLesson('');
+        setSelectedScenario(scenario);
+    }
+
+    const handleLessonClick = (lesson) => {
+        setShowLessons(false);
+        setShowScenarios(false);
+        setSelectedLesson(lesson);
+    }
+
+    useEffect(() => {
+    const lessonTable = []
+
+    for(let i = 0; i < getScenarioLength(); i++) {
+        if (getScenario(i).name === selectedScenario) {
+            for(let j =0; j < getScenario(i).subSections.length; j++) {
+                lessonTable.push(getScenario(i).subSections[j]);
+            }
+            break;
+        }
+    }
+    setLessons(lessonTable);
+    }, [selectedScenario])
+    
+    const navigate = useNavigate();      
     return(
         <div className="whole-page">
-            <div className="block">
-                <h1 className="header">Pawn</h1>
-                <ul className="list">
-                    <li onClick={() => navigate("/lessons", {state: {scenario: "Pawn", lesson: "Basic"}})}>Basic</li>
-                    <li onClick={() => navigate("/lessons", {state: {scenario: "Pawn", lesson: "Capture"}})}>Capture</li>
-                    <li onClick={() => navigate("/lessons", {state: {scenario: "Pawn", lesson: "Training 1"}})}>Training 1</li>
-                    <li>Training 2</li>
-                    <li>Training 3</li>
-                    <li>Special Move</li>
-                </ul>
+            <div className="selector scenario-selector" onClick={() => {
+                setShowScenarios(!showScenarios);
+            }}>
+                {selectedScenario || "Select a scenario."}
             </div>
-            <div className="block">
-                <h1 className="header">Bishop</h1>
-                <ul className="list">
-                    <li>The Basic</li>
-                    <li>Training 1</li>
-                    <li>Training 2</li>
-                    <li>Training 3</li>
-                    <li>Training 4</li>
-                    <li>Final</li>
-                </ul>
+            
+            {showScenarios && (
+                <div className="container scenario-container">
+                    {scenarios.map((scenarioItem, index) => (
+                        <ScenarioTemplate key={index} scenario={scenarioItem} onClick={() => handleScenarioClick(scenarioItem.name)}/>
+                        // Use onClick = {() => function} if argument is needed, otherwise onClick = {function}.
+                    ))}
+                </div>
+            )}
+            
+            <div className="selector lesson-selector" onClick={() => {
+                setShowLessons(!showLessons);
+            }}>
+                {selectedLesson || "Select a lesson."}
             </div>
-            <div className="block">
-                <h1 className="header">Knight</h1>
-                <ul className="list">
-                    <li>The Basic</li>
-                    <li>Training 1</li>
-                    <li>Training 2</li>
-                    <li>Training 3</li>
-                    <li>Training 4</li>
-                    <li>Final</li>
-                </ul>
-            </div>
-            <div className="block">
-                <h1 className="header">Rook</h1>
-                <ul className="list">
-                    <li>The Basic</li>
-                    <li>Training 1</li>
-                    <li>Training 2</li>
-                    <li>Training 3</li>
-                    <li>Training 4</li>
-                    <li>Final</li>
-                </ul>
-            </div>
-            <div className="block">
-                <h1 className="header">Queen</h1>
-                <ul className="list">
-                    <li>The Basic</li>
-                    <li>Training 1</li>
-                    <li>Training 2</li>
-                    <li>Training 3</li>
-                    <li>Final</li>
-                </ul>
-            </div>
+            {showLessons && (
+                <div className="container scenario-container">
+                    {lessons.map((lessonItem, index) => (
+                        <LessonTemplate key={index} lesson={lessonItem} onClick={() => handleLessonClick(lessonItem.name)}/>
+                    ))}
+                </div>
+            )}
+
+            <button className="enterInfo" onClick={() => {
+                navigate("/lessons", {state: {scenario: selectedScenario, lesson: selectedLesson}});
+            }}>
+                Go!
+            </button>
         </div>
     );
 }
