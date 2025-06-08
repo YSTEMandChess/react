05467f7c66cca5f66a75f0fff20050e397a965c0
Eli Boyden <aqu4eb@virginia.edu>
2025-06-08 19:35:30 -0400

    added styling for header of MentorProfileRefactor

--- react-ystemandchess/src/Pages/Mentor-Profile/MentorProfileRefactor.tsx
+++ react-ystemandchess/src/Pages/Mentor-Profile/MentorProfileRefactor.tsx
@@ -97,21 +97,24 @@ export default function MentorProfileRefactor() {
 
     return (
         <>
-        <div className="mentor-profile">
+            <div className="mentor-profile">
             <div className="mentor-profile-header">
-              { profilePicture ? (
-                <img src={profilePicture} alt="Profile" className="mentor-profile-picture" />
-              ) : (
-                <div className="mentor-profile-placeholder">
-                    {// @ts-ignore 
-                        // if someone can figure out how to make this work without the ts-ignore, please do so
-                        <UserIcon className="mentor-profile-icon" />
-                    }
-                </div>
-              )}
-              <h2>Hello, {firstName} {lastName}!</h2>
+                {profilePicture ? (
+                <img
+                    src={profilePicture}
+                    alt="Profile"
+                    className="mentor-profile-picture" // When a profile picture updoad is supported, it can be styled with this class
+                />
+                ) : (
+                <>
+                    {// @ts-ignore
+                    // If someone can figure out how to make this work without the ts-ignore, please do so
+                    <UserIcon className="mentor-placeholder-icon" /> }
+                </>
+                )}
+                <h2 className="greeting">Hello, {firstName} {lastName}!</h2>
             </div>
-        </div>
+            </div>
         </>
     )
 }
