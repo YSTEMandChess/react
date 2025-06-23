91b53bade9d5a0714edd6ac2d74bea671e7d031d
Eli Boyden <aqu4eb@virginia.edu>
2025-06-23 11:15:24 -0400

    Refactored sponsors and partners page

--- react-ystemandchess/src/Pages/About-Us/SponsorsPartners/SponsorsPartners.tsx
+++ react-ystemandchess/src/Pages/About-Us/SponsorsPartners/SponsorsPartners.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./SponsorsPartners.scss";
+import LogoLineBreak from "../../../images/LogoLineBreak.png";
 import sponsor1 from "../../../images/sponsors/ventive.png";
 import sponsor2 from "../../../images/sponsors/kount.png";
 import sponsor3 from "../../../images/sponsors/idahoCentral.png";
+import sponsor4 from "../../../images/sponsors/PH.svg";
 import partner1 from "../../../images/partners/boiseRescue.png";
 import partner2 from "../../../images/partners/boiseDistrict.png";
 import partner3 from "../../../images/partners/boysAndGirls.png";
@@ -10,9 +12,19 @@ import partner4 from "../../../images/partners/possible.png";
 import partner5 from "../../../images/partners/Rotary.png";
 
 const SponsorsPartners = () => {
+
   return (
     <div className="sponsors-partners">
       <h1>Sponsors & Partners</h1>
+      <p>
+        We are grateful for the support of our sponsors and partners who help
+        make our mission possible. Their contributions enable us to provide
+        resources, training, and opportunities for the youth in our community.
+        Thank you for your generosity and commitment to making a difference!
+      </p>
+      <figure className="logo-break">
+        <img src={LogoLineBreak} alt="" role="presentaion"/> 
+      </figure>  
 
       <div className="sponsors-partners-section">
         <h2>Sponsors</h2>
@@ -20,6 +32,7 @@ const SponsorsPartners = () => {
           <img src={sponsor1} alt="Sponsor 1" />
           <img src={sponsor2} alt="Sponsor 2" />
           <img src={sponsor3} alt="Sponsor 3" />
+          <img src={sponsor4} alt="Sponsor 4" />
         </div>
       </div>
 
