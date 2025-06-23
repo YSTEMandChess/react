89f97cd2b2dd6f43d29eb00b32a32b16688285e4
Eli Boyden <aqu4eb@virginia.edu>
2025-06-23 11:22:54 -0400

    added accessibility to SponsorPartners page

--- react-ystemandchess/src/Pages/About-Us/SponsorsPartners/SponsorsPartners.tsx
+++ react-ystemandchess/src/Pages/About-Us/SponsorsPartners/SponsorsPartners.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./SponsorsPartners.scss";
 import LogoLineBreak from "../../../images/LogoLineBreak.png";
-import sponsor1 from "../../../images/sponsors/ventive.png";
-import sponsor2 from "../../../images/sponsors/kount.png";
-import sponsor3 from "../../../images/sponsors/idahoCentral.png";
-import sponsor4 from "../../../images/sponsors/PH.svg";
-import partner1 from "../../../images/partners/boiseRescue.png";
-import partner2 from "../../../images/partners/boiseDistrict.png";
-import partner3 from "../../../images/partners/boysAndGirls.png";
-import partner4 from "../../../images/partners/possible.png";
-import partner5 from "../../../images/partners/Rotary.png";
+import ventive from "../../../images/sponsors/ventive.png";
+import kount from "../../../images/sponsors/kount.png";
+import idahoCentral from "../../../images/sponsors/idahoCentral.png";
+import PH from "../../../images/sponsors/PH.svg";
+import boiseRescue from "../../../images/partners/boiseRescue.png";
+import boiseDistrict from "../../../images/partners/boiseDistrict.png";
+import boysAndGirls from "../../../images/partners/boysAndGirls.png";
+import possible from "../../../images/partners/possible.png";
+import rotary from "../../../images/partners/Rotary.png";
 
 const SponsorsPartners = () => {
 
   return (
-    <div className="sponsors-partners">
+    <main role="main" className="sponsors-partners">
       <h1>Sponsors & Partners</h1>
       <p>
         We are grateful for the support of our sponsors and partners who help
@@ -26,27 +26,27 @@ const SponsorsPartners = () => {
         <img src={LogoLineBreak} alt="" role="presentaion"/> 
       </figure>  
 
-      <div className="sponsors-partners-section">
+      <section className="sponsors-partners-section" role="region">
         <h2>Sponsors</h2>
-        <div className="logos">
-          <img src={sponsor1} alt="Sponsor 1" />
-          <img src={sponsor2} alt="Sponsor 2" />
-          <img src={sponsor3} alt="Sponsor 3" />
-          <img src={sponsor4} alt="Sponsor 4" />
+        <div className="logos" aria-label="Sponsors logos">
+          <img src={ventive} alt="Ventive" />
+          <img src={kount} alt="Kount" />
+          <img src={idahoCentral} alt="Idaho Central Credit Union" />
+          <img src={PH} alt="Partner Hero (PH)" />
         </div>
-      </div>
+      </section>
 
-      <div className="sponsors-partners-section">
+      <section className="sponsors-partners-section" role="region">
         <h2>Partners</h2>
-        <div className="logos">
-          <img src={partner1} alt="Partner 1" />
-          <img src={partner2} alt="Partner 2" />
-          <img src={partner3} alt="Partner 3" />
-          <img src={partner4} alt="Partner 4" />
-          <img src={partner5} alt="Partner 5" />
+        <div className="logos" aria-label="Partners logos">
+          <img src={boiseRescue} alt="Boise Rescue Misison Ministries" />
+          <img src={boiseDistrict} alt="Boise District Community Schools" />
+          <img src={boysAndGirls} alt="Boys and Girls Clubs of Ada County" />
+          <img src={possible} alt="Everything's Possible" />
+          <img src={rotary} alt="Boise Sunrise Rotary Club" />
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
