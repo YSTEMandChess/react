e6091e9ea05a6c987fb019bb7c229271e569ab3b
Brandon Yu <yubrandon@debian.debian.net>
2025-09-16 22:41:52 -0700

    added backend routes for updating activities

--- middlewareNode/routes/activities.js
+++ middlewareNode/routes/activities.js
@@ -17,13 +17,13 @@ async function getDb() {
   return cachedClient.db("ystem"); // returned cached client
 }
 
-router.get("/:username", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
         const db = await getDb();
         const username = req.params.username;
         
         if(!username) {
-            res.status(401).json({error:'Authentication required'})
+            res.status(401).json({error:'Authentication required'});
         }
         const users = db.collection("users");
         const currentUser = await users.findOne(
@@ -44,5 +44,65 @@ router.get("/:username", async (req, res) => {
         res.status(500).json({error: 'Server error'});
     }
 })
+
+
+router.put("/activity/:activityName", async (req, res) => {
+    try {
+        const db = await getDb();
+        const { username, activityName } = req.params;
+        if(!username) {
+            res.status(401).json({error:'Authentication required'});
+        }
+        const users = db.collection("users");
+        const currentUser = await users.findOne(
+            { username }, 
+        );
+        if(!currentUser) {
+            res.status(404).json({error:"User not found!"});
+        }
+        const userId = currentUser._id;
+        //use userId as parameter to select document to update
+            //pass activity name in reqeust to update to completed
+        const activities = db.collection("activities");
+        /*await activities.updateOne(
+            { userId, "activities.name": activityName },
+            { $set: { "activities.$.completed": true } }
+        );*/
+        res.status(200);
+    } catch (err) {
+        console.error('Error updating activities: ', err);
+        res.status(500).json({error: 'Server error'});
+    }
+})
+
+router.post("/activity/complete", async (req, res) => {
+    try {
+        const db = await getDb();
+        const { username } = req.params;
+        if(!username) {
+            res.status(401).json({error:'Authentication required'});
+        }
+        const users = db.collection("users");
+        const currentUser = await users.findOne(
+            { username }, 
+        );
+        if(!currentUser) {
+            res.status(404).json({error:"User not found!"});
+        }
+        const userId = currentUser._id;
+        //use userId as parameter to select document to update
+            //pass activity name in reqeust to update to completed
+        const activities = db.collection("activities");
+        /*await activities.updateOne(
+            { userId },
+            { $push: { lastCompleted: new Date() } }
+        );*/
+        res.status(200);
+    } catch (err) {
+        console.error('Error updating activity completion: ', err);
+        res.status(500).json({error: 'Server error'});
+    }
+})
+
 
 module.exports = router;
