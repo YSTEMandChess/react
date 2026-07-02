142efcf26f3d411c1703c1730b6e1f53443f850c
div <rasaniadivya1@gmail.com>
2026-07-02 18:26:58 +0000

    Refactor environment files and update .gitignore; improve PlayComputer component structure and logic

--- react-ystemandchess/src/environments/environment.prod.js
+++ react-ystemandchess/src/environments/environment.prod.js
@@ -1,11 +1,12 @@
 export const environment = {
-	production: false,
-	agora: {
-		appId: '6c368b93b82a4b3e9fb8e57da830f2a4',
-	},
-	urls: {
-		middlewareURL: 'http://localhost/middleware/',
-		stockfishServerURL: 'http://localhost/stockfishserver/',
-		chessServerURL: 'http://localhost/chessserver/',
-	},
+  production: false,
+  agora: {
+    appId: "6c368b93b82a4b3e9fb8e57da830f2a4",
+  },
+  urls: {
+    middlewareURL: "http://localhost/middleware/",
+    chessClientURL: "http://localhost/chessclient/",
+    stockFishURL: "http://localhost/stockfishserver/",
+    chessServer: "http://localhost/chessserver/",
+  },
 };
