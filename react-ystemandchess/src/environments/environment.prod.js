228ee3fa7c45236df6ce4f4b2b2fc364361038b4
sweksha-cloud <swekshas123@gmail.com>
2026-08-14 04:24:09 -0700

    Fix frontend production environment config

--- react-ystemandchess/src/environments/environment.prod.js
+++ react-ystemandchess/src/environments/environment.prod.js
@@ -1,12 +1,11 @@
 export const environment = {
-	production: false,
+	production: true,
 	agora: {
 		appId: '6c368b93b82a4b3e9fb8e57da830f2a4',
 	},
 	urls: {
 		middlewareURL: 'http://localhost/middleware/',
 		stockfishServerURL: 'http://localhost/stockfishserver/',
-		chessClientURL: 'http://localhost/chessclient/',
-		chessServer: 'http://localhost/chessserver/',
+		chessServerURL: 'http://localhost/chessserver/',
 	},
 };
